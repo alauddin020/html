7e2c4d0bb90219d4e7a75c493d3e13c356442805
alauddin020 <alauddin020@gmail.com>
2020-11-24 18:43:25 +0600

    Inital

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\User;
+
+class UserObserver
+{
+    private $request;
+    public function __construct()
+    {
+        $this->request =\request()->all();
+    }
+
+    public function created(User $user)
+    {
+        dd($this->request);
+    }
+
+    /**
+     * Handle the user "updated" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "restored" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "force deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
